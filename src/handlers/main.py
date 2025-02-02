d52863b41be059f0ee0d139bf9e801117b3f0271
Ivan <ovchinno.ivan@gmail.com>
2025-02-02 22:39:00 +0300

    [FEATURE] - Add web_app command

--- src/handlers/main.py
+++ src/handlers/main.py
@@ -1,11 +1,31 @@
 from aiogram import Router
-from aiogram.types import Message
+from aiogram.filters import Command
+from aiogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup, WebAppInfo
 import logging
 
 
 router = Router()
 
 logger = logging.getLogger(__name__)
+
+
+@router.message(Command('web_app'))
+async def web_app(message: Message) -> None:
+    _, web_app_url = message.text.split()
+
+    await message.answer(
+        text='Web app',
+        reply_markup=InlineKeyboardMarkup(
+            inline_keyboard=[
+                [
+                    InlineKeyboardButton(
+                        text='Open web app',
+                        web_app=WebAppInfo(url=web_app_url),
+                    )
+                ],
+            ],
+        ),
+    )
 
 
 @router.message()
